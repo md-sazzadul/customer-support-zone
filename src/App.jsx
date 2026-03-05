8b5c19b66dc2f2d6bf918d589c7ac38f4b2842db
md-sazzadul <md.sazzadul.islam15@gmail.com>
2026-03-05 11:16:25 +0100

    include length of taskStatusItems and resolvedItems in the banner count to show the count of In-Progress and Resolved tickets

--- src/App.jsx
+++ src/App.jsx
@@ -23,8 +23,16 @@ function App() {
 
         <div className="max-w-7xl mx-auto px-6 py-8">
           <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mb-10">
-            <Banner title="In-Progress" count={0} type="progress" />
-            <Banner title="Resolved" count={0} type="resolved" />
+            <Banner
+              title="In-Progress"
+              count={taskStatusItems.length}
+              type="progress"
+            />
+            <Banner
+              title="Resolved"
+              count={resolvedItems.length}
+              type="resolved"
+            />
           </div>
         </div>
 
